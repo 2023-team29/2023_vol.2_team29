226536e223c3fc05bc8d2d622f145a4a418c2378
toshi <93854575+Toshi-Noguchi@users.noreply.github.com>
2023-04-15 19:22:43 +0900

    ローカル変数の追加

--- app/source/quiz/views.py
+++ app/source/quiz/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 from .forms import AnswerForm
 # Create your views here.
 
+#問題と解答のリスト
+questions = []
+answers = []
+#問題数のカウント
+number = 0
+
 def home(request):
     """
     ホーム画面の表示
@@ -14,11 +20,15 @@ def match(request):
     """
     クイズ
     """
+    #chatGPT APIの実装
+    
+
+
     form = AnswerForm()
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
-           answer = form.cleaned_data['subject'] 
+           answer = form.cleaned_data['answer'] 
 
 
     else:
